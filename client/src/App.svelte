98f90d2ff77fd305adb71303bbbcc1eefa73d4d2
Lauras5 <idlaurasalcido@gmail.com>
2021-02-13 21:22:23 -0800

    pdf

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,22 +1,53 @@
 <script>
-	import {onMount} from 'svelte';
+    import {onMount} from 'svelte';
 
-	let data = {};
+    let data = [];
+    let dataLen;
+    let fileIndex = 0;
+    let activePdf;
+    let pdfTitle;
 
-	onMount(async () => {
-		const res = await fetch('/api');
-		data = await res.json();
-		console.log(data);
-	});
+    onMount(async () => {
+        const res = await fetch('/api');
+        data = await res.json();
+        console.log(data)
+        dataLen = data.data.length;
+        updatePage();
+    });
 
+    function nextPage() {
+        if (fileIndex == (dataLen - 1))
+            fileIndex = 0;
+        else
+            fileIndex++;
+
+        updatePage();
+    }
+
+    function prevPage() {
+        if (fileIndex == 0)
+            fileIndex = dataLen - 1;
+        else
+            fileIndex--;
+
+        updatePage();
+    }
+
+    function updatePage() {
+        pdfTitle = data.data[fileIndex].pdf;
+        activePdf = data.data[fileIndex].file_location;
+    }
 
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+    h1 {
+        color: purple;
+    }
 </style>
 
 <h1>Hello world!</h1>
-<div>Data: {JSON.stringify(data)}</div>
+
+<iframe title={pdfTitle} src={activePdf} height="600px" width="100%"></iframe>
+<button on:click={prevPage}>Previous</button>
+<button on:click={nextPage}>Next</button>
