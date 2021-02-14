f52ff201d2664839235b217d55e96f19ee7aee66
ALMaclaine <almaclaine@gmail.com>
2021-02-14 15:28:51 -0600

    accept multiple tags

--- src/api/tag.js
+++ src/api/tag.js
@@ -2,4 +2,6 @@ const {getTagScopedRouter, setupTagSystem} = require('sql-tag-system-koa-route')
 const dbInfo = require('../dbInfo');
 
 setupTagSystem(dbInfo).then(() => {});
-module.exports = getTagScopedRouter(dbInfo);
+const router = getTagScopedRouter(dbInfo);
+
+module.exports = router;
